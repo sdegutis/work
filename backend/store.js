a23bf7eb2ec7ccd59cd426cca0de6b55c0e8ced6
Steven Degutis <sbdegutis@gmail.com>
2021-05-22 05:56:04 -0500

    Store

---
+++ backend/store.js
@@ -0,0 +1,30 @@
+const electron = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+const datadir = electron.app.getPath('userData');
+const filepath = path.join(datadir, 'data.json');
+
+module.exports.data = {};
+
+if (fs.existsSync(filepath)) {
+  const content = fs.readFileSync(filepath, 'utf8');
+  module.exports.data = JSON.parse(content);
+}
+
+function saveImmediately() {
+  fs.writeFileSync(filepath, JSON.stringify(module.exports.data, null, 2));
+  console.log('saved data');
+}
+
+let saveTimeout;
+function saveSoon() {
+  clearTimeout(saveTimeout);
+  saveTimeout = setTimeout(saveImmediately, 1000);
+}
+
+module.exports.save = saveSoon;
+module.exports.saveImmediately = saveImmediately;
+
+setInterval(saveSoon, 1000 * 30);
+saveImmediately();
