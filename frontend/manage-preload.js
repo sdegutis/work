c5ce1dceda729734f5c5d94fec6c725c480e88d7
Steven Degutis <sbdegutis@gmail.com>
2021-05-24 02:43:20 -0500

    Removing now-ambiguous commentary.

--- frontend/manage-preload.js
+++ frontend/manage-preload.js
@@ -1,7 +1,5 @@
 const electron = require('electron');
 
-// There's got to be a less convoluted way to do this...
-
 let ready;
 electron.contextBridge.exposeInMainWorld('main', {
   ready(fn) { ready = fn },
