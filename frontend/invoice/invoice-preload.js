302d5d6a601ec7e9e22a53de6486c487a2b6e84e
Steven Degutis <sbdegutis@gmail.com>
2021-05-24 05:41:01 -0500

    Hide invoice lines with no hours worked.

--- frontend/invoice/invoice-preload.js
+++ frontend/invoice/invoice-preload.js
@@ -12,13 +12,16 @@ electron.contextBridge.exposeInMainWorld('main', {
   transform: async (text) => {
     const data = await electron.ipcRenderer.invoke('get-data');
 
-    const lines = data.tasks.map(({ name, seconds }) => {
-      const hours = seconds / 60 / 60;
-      const time = roundToNearest15Mins(hours);
-      const rate = data.rate;
-      const total = time * rate;
-      return ({ name, time, rate, total });
-    });
+    const lines = (data.tasks
+      .map(({ name, seconds }) => {
+        const hours = seconds / 60 / 60;
+        const time = roundToNearest15Mins(hours);
+        const rate = data.rate;
+        const total = time * rate;
+        return ({ name, time, rate, total });
+      })
+      .filter(({ total }) => total > 0)
+    );
 
     const total = (lines
       .map(line => line.total)
