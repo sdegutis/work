d8325ab86a505b68170a1f3081e263b4835b142c
Steven Degutis <sbdegutis@gmail.com>
2021-05-24 04:45:16 -0500

    Preview constantly, generate-button will only save.

--- frontend/invoice/invoice.js
+++ frontend/invoice/invoice.js
@@ -17,7 +17,24 @@ main.ready((data) => {
   editorEl.value = data.template;
   editorEl.oninput = () => {
     main.set('template', editorEl.value);
+    previewSoon();
   };
+
+  let previewTimer;
+  function previewSoon() {
+    if (!previewTimer) {
+      previewTimer = setTimeout(previewImmediately, 250);
+    }
+  }
+
+  function previewImmediately() {
+    previewTimer = undefined;
+    main.transform(editorEl.value).then(html => {
+      previewEl.srcdoc = html;
+    });
+  }
+
+  previewImmediately();
 
   window.addEventListener('resize', resizePreview);
   resizePreview();
@@ -25,9 +42,7 @@ main.ready((data) => {
   generateButton.onclick = (e) => {
     e.preventDefault();
 
-    main.transform(editorEl.value).then(html => {
-      previewEl.srcdoc = html;
-    });
+    // TODO: save PDF with save dialog
   };
 });
 
